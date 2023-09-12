16baa8b5cf6ce5d46df15b9ee066c8562418e25f
Aymenbs22 <aymenbenslima0@gmail.com>
2023-09-12 15:45:53 +0100

    Modify the component to use Aphrodite within the js file Define the styling for the margin within the file Delete the file BodySection.css and the import

--- React_inline_styling/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.js
+++ React_inline_styling/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -1,8 +1,14 @@
-import './BodySection.css';
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+    bodySectionWithMargin: {
+        marginBottom: '40px'
+  }
+  })
 
 function BodySectionWithMarginBottom(props) {
     return (
-        <div className="bodySectionWithMargin">
+        <div className={css(styles.bodySectionWithMargin)}>
         <BodySection>
         {props}
         </BodySection>
