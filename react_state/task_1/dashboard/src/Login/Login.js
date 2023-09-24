0a5a3a019345a8d18295fc2bff8d467ba5bd8193
Aymenbs22 <aymenbenslima0@gmail.com>
2023-09-24 15:30:15 +0100

    Create a local state with the value isLoggedIn set to false Wrap the inputs within a form element Replace the button by an input element with type submit Create a function named handleLoginSubmit that will update the local state by setting isLoggedIn to true When the form is submitted call the newly created login submission handling function create controlled components in the Login component intask_1/dashboard/src/Login/Login.js
    Modify the local state to add two new values email and password. By default these values are empty but not null
    Modify the two inputs elements in the form and set their values to use the local state
    Create two function handleChangeEmail and handleChangePassword that the two inputs will call when the value in the input field is changed. The local state should update with what the user is typing
    modify state callback in task_1/dashboard/src/Login/Login.js
    
    Modify the local state to add one value enableSubmit. It should be set to false by default
    Modify the Submit button to only be enabled when the enableSubmit value of the local state is true
    Every time the user changes the value of the email or password field, verify that both fields are not empty. If they are not empty, you can enable the submit button
    add tests in the task_1/dashboard/src/Login/Login.js suite
    
    Add a test to verify that the submit button is disabled by default
    Add a test to verify that after changing the value of the two inputs, the button is enabled

---
+++ react_state/task_1/dashboard/src/Login/Login.js
@@ -0,0 +1,45 @@
+import { getFullYear,getFooterCopy } from './utils';
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+    bodySectionWithMargin: {
+        marginBottom: '40px'
+  }
+  })
+
+const [isLoggedIn] = useState(false);
+const [email] = useState('');
+const [password] = useState('');
+const [enableSubmit] = useState(false);
+
+const handleLoginSubmit = (e) => {
+  e.preventDefault();
+  isLoggedIn(true);
+};
+
+function handleChangeEmail(e) {
+  setState({ email: e.target.value });
+  enableSubmit(newEmail.length > 0 && password.length > 0);
+}
+
+function handleChangePassword(e) {
+  setState({ password: e.target.value });
+  enableSubmit(email.length > 0 && newPassword.length > 0);
+}
+
+function login() {
+  return (
+      <body className="App-body">
+      <p>Login to access the full dashboard</p>
+      <label>Email: </label>
+      <form>
+      <input type='email' value={email}></input>
+      <label>Password: </label>
+      <input type='password' value={password}></input>
+      </form>
+      <input type="submit" onSubmit={this.handleLoginSubmit} disabled={!enableSubmit}></input>
+      </body>
+  );
+}
+
+export default login;
